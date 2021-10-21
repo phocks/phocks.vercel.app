6d586e81551c27a2c7f2e45fa2ff6aff0cb1fe69
Joshua Byrd <phocks@gmail.com>
2021-10-21 13:35:47 +1000

    MVP timeline

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,69 +1,85 @@
 import Head from "next/head";
 import { useEffect } from "react";
-import useSWR from "swr";
 import * as dayjs from "dayjs";
 var relativeTime = require("dayjs/plugin/relativeTime");
 dayjs.extend(relativeTime);
 
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Timeline.module.css";
 
-export default function Home() {
-  const fetcher = (...urls) => {
-      console.log(urls);
-    const f = (u) => fetch(u).then((r) => r.json());
+const feedsToFetch = [
+  "https://phocks.vercel.app/feed.json",
+  "https://ash.ms/micro.json",
+];
 
-  if (urls.length > 1) {
-    return Promise.all(urls.map(f));
-  }
-  return f(urls);
-  };
-  const { data, error } = useSWR(
-    ["https://phocks.vercel.app/feed.json",
-    "https://ash.ms/micro.json"],
-    fetcher
+export async function getServerSideProps() {
+  var data = await Promise.all(
+    feedsToFetch.map(async (url) => {
+      const resp = await fetch(url);
+      const feedObject = await resp.json();
+      const finalFeed = { fetched_url: url, ...feedObject };
+      console.log(finalFeed);
+      return finalFeed;
+    })
   );
 
-  console.log(data)
+  // Pass data to the page via props
+  return { props: { data } };
+}
 
-  const { items, title } = data || [];
+export default function Timeline({ data }) {
+  const getItems = () => {
+    const itemsArray = data.map((d) => {
+      const { items, title, fetched_url } = d;
 
-  useEffect(() => {}, []);
+      const itemsWithSource = items.map((item) => {
+        return { title, fetched_url, ...item };
+      });
+
+      return itemsWithSource;
+    });
+
+    const combinedItems = itemsArray.flat(1);
+
+    const sortedCombinedItems = combinedItems.sort((a, b) => {
+      // return b.date_published - a.date_published
+
+      return new Date(b.date_published) - new Date(a.date_published);
+    });
+    return { items: sortedCombinedItems };
+  };
+
+  const { items } = getItems();
 
   return (
     <div className={styles.container}>
       <Head></Head>
 
-      {error && <div>failed to load</div>}
+      <>
+        <h1>phocks's timeline</h1>
+        <div className={styles.subtitle}>
+          Updates from people I follow. Repo{" "}
+          <a href="https://github.com/phocks/phocks.vercel.app">here</a>. Feed{" "}
+          <a href="/feed.json">here</a>. Uses{" "}
+          <a href="https://www.jsonfeed.org/">JSON Feed</a> for syndication.
+        </div>
 
-      {!data ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h1>{title}</h1>
-          <div className={styles.subtitle}>
-            {data?.description} Repo{" "}
-            <a href="https://github.com/phocks/phocks.vercel.app">here</a>. Feed{" "}
-            <a href="/feed.json">here</a>. Uses{" "}
-            <a href="https://www.jsonfeed.org/">JSON Feed</a> for syndication.
-          </div>
-          <div className={styles.posts}>
-            {items?.map((item, iteration) => {
-              const { date_published, content_text } = item;
+        <div className={styles.posts}>
+          {items?.map((item, iteration) => {
+            const { date_published, content_text, title, fetched_url } = item;
 
-              return (
-                <div className={styles.item} key={iteration}>
-                  <div className={styles.date}>
-                    <abbr title={date_published}>
-                      {dayjs(date_published).fromNow()}
-                    </abbr>
-                  </div>
-                  {content_text}
+            return (
+              <div className={styles.item} key={iteration}>
+                <div className={styles.date}>
+                  <abbr title={date_published}>
+                    {dayjs(date_published).fromNow()}
+                  </abbr> &middot; <a className={styles.source} href={fetched_url}>{title}</a>
                 </div>
-              );
-            })}
-          </div>
-        </>
-      )}
+                {content_text}
+              </div>
+            );
+          })}
+        </div>
+      </>
     </div>
   );
 }
